3d9ea7fbb9834e438b19ff3148c7c3c8cd7129b8
[Jackson Bond] <jacksonbond11@yahoo.com>
2024-01-02 18:42:53 -0600

    update year - happy new year!

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer text-white p-4 bottom-0 inset-x-0">
-      <h3>© 2023 VegainRecipes. All rights reserved.</h3>
+      <h3>© 2024 VegainRecipes. All rights reserved.</h3>
     </footer>
   );
 };
